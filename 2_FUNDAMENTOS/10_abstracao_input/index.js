caf7df206b4504b19d0a3c6da6f317b923c6d8df
denervalspelizario <denervalspelizario@gmail.com>
2022-12-30 06:28:14 -0300

    estudo node input de dado usando inquirer

--- 2_FUNDAMENTOS/10_abstracao_input/index.js
+++ 2_FUNDAMENTOS/10_abstracao_input/index.js
@@ -1,3 +1,22 @@
-const inquirer = require('inquirer')  // exportando o inquirer  obs- tem que instalar npm install inquirer
+const inquirer = require('inquirer')  // exportando o inquirer  obs- tem que instalar npm install inquirer  
+                                      //comando que faz funcionar o inquirer com require  é o  npm install --save inquirer@^8.0.0  
 
-inquirer.prompt([{},{}]) // metodo responsavel por fazer as perguntas, dentro de um array e cada pergunta dentro de cada parenteses
+inquirer.prompt([
+    {
+        name: 'p1',  // vai receber a nota
+        message: 'Qual a primeira nota?'            // perguntas
+    },
+    {
+        name: 'p2', // vai receber a nota
+        message: 'Qual a segunda nota?'   // perguntas
+    },
+])
+.then((answers) => {  //respostas - then(entao) - sempre que der certo nas perguntas desce para o then(então) que manipula a resposta 
+    console.log(answers) // imprimindo a resposta
+    const media = ((parseInt(answers.p1) + parseInt(answers.p2)) / 2 )  // calculando média  usando parseInt pois recebemos os dados como string  e ele transforma em inteiro
+
+    console.log(`A média é: ${media}`) // imprimindo a média
+})  
+.catch(err => console.log(err))  // catch - sempre que houve algum erro vai cair no catch
+
+   // metodo responsavel por fazer as perguntas, dentro de um array e cada pergunta dentro de cada parenteses
--- 2_FUNDAMENTOS/9_lendo_input/index.js
+++ 2_FUNDAMENTOS/9_lendo_input/index.js
@@ -1,22 +1,18 @@
 const readline = require('readline').createInterface({  // importando o readline e ja executando usando o createInterface
 
-    input: process.stdin,  // configurtações para receber dados e enviar dados aos usuários
-    output: process.stdout,
+    input: process.stdin,  // configurações para receber dados  aos usuários
+    output: process.stdout, // configurações para  enviar dados aos usuários
 
 })
 
 
-readline.question("Qual a sua linguagem preferida? ", (language) => {   // a arrow function é o segundo argumento que vai trabalhart com o segundo dado da pergunta
+readline.question("Qual a sua linguagem preferida? ", (language) => {   // a arrow function é o segundo argumento que vai trabalhar com o segundo dado da pergunta
   
-    if(language === 'php'){
-        
+    if(language === 'php'){   
         console.log("Isso ném é linguagem")
-    
     } else {
-
         console.log(`A minha linguagem preferida é ${language}`)
     }
-  
     
     readline.close()  // fechando a execução
 })
