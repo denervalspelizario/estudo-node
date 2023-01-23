4335896640fc4203eec3121878230eaf345b8480
denervalspelizario <denervalspelizario@gmail.com>
2023-01-23 11:48:07 -0300

    estudo node revisao de import e export

--- 2_FUNDAMENTOS/1_modulo_interno/index.js
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -2,9 +2,15 @@
 const meuModulo = require('./meu_modulo') // require busca o arquivo - obs = nao precisa passar a extensão
 const soma = meuModulo.soma   // puxando a funcao soma la do arquivo meu_modulo.js na const soma
 
+const subtracao = meuModulo.subtracao // puxando a funcao subtracao la do arquivo meu_modulo.js na const subtracao
+                                      // não precisa ali no meuModulo.subtracao o () pq isso chamaria a funcao naquele momento e estamos apenas exportando a infomação para a
+                                      // a const subtracao              
+
 
 soma(2,3)
 soma(5,10)
+subtracao(10,5)
+subtracao(5,3)
 
 
 
--- 2_FUNDAMENTOS/1_modulo_interno/meu_modulo.js
+++ 2_FUNDAMENTOS/1_modulo_interno/meu_modulo.js
@@ -1,6 +1,11 @@
 
 module.exports = {     // maneira de exportar uma função
-     soma(a, b){       // para exportar uma função não precisa colocar function      
+    soma(a, b){       // para exportar uma função não precisa colocar function      
         console.log(a + b)
     },
+    subtracao(x, z){  // pode-se exportar dentro do modulo quantas funções quiser
+        console.log(x-z)
+    },
+
 }
+
