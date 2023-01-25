a00ed5c522e528176596023849952cad9b795547
denervalspelizario <denervalspelizario@gmail.com>
2023-01-25 09:43:39 -0300

    estudo node revisao core modules e ler argumentos

--- 2_FUNDAMENTOS/3_core_module/index.js
+++ 2_FUNDAMENTOS/3_core_module/index.js
@@ -1,6 +1,10 @@
 
 const path = require('path')  // chamando o modulo path
 
-const extension = path.extname("arquivo.php") //  jogando na variavel extension o tipo de extensão do arquivo php
+const extension = path.extname("arquivo.php") //  jogando na variavel extension o  extname analisa qual o tipo de arquivo que no caso vai ser o php
+const extension2 = path.extname("arquivo.pdf")
+const extension3 = path.extname("arquivo.doc")
 
 console.log(extension)
+console.log(extension2)
+console.log(extension3)
--- 2_FUNDAMENTOS/4_ler_argumentos/index.js
+++ 2_FUNDAMENTOS/4_ler_argumentos/index.js
@@ -1,10 +1,13 @@
 // nome
 
-console.log(process.argv) // dando consoleg com argumento nome=Denerval
+// passar 2 argumentos nome='nome qualquer' idade='idade qualquer' 
+// process.argv mostra os argumentos executados que no caso são 5 neste exemplo
 
-const args = process.argv.slice(2)  
+console.log(process.argv) // dando consoleg com argumento nome=Denerval  ou seja  node .\index.js nome=Denerval
 
-console.log(args)
+const args = process.argv.slice(2)  // atravez do slice  ele me da o terceiro argumento(lembre se começa 0-3) pois tem 3 argumentos 
+
+console.log(args) // na variavel args só consta o terceiro argumento que no caso é nome=Denerval
 
 const nome = args[0].split("=")[1] // paga o argumento  0 e usa o split para divir em 2  e depois pega o indice 1 (o segundo) que n caso é o nome 
 
